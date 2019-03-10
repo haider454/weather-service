bc4c24d6db1652d007cb8ac48aa9c351a8001ec4
Sharjeel Haider <haidersharjeel454@gmail.com>
2019-03-09 23:14:12 -0500

    Added about text

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Thomas the dank engine'
+        name: 'Sharjeel Haider'
     })
 })
 
